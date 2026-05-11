b1a8926912f20fd720f059ceb3268210898cfd20
romainsessa <romainsessa@gmail.com>
2026-05-11 16:39:04 +0200

    Fix Pie Chart display issue

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild } from '@angular/core';
 import Chart from 'chart.js/auto';
 
 @Component({
